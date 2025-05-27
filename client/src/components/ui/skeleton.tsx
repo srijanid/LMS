3a438885fe024139418a4dda4e2e2e2f3acd2dec
srijanid <srijanidas06@gmail.com>
2025-05-27 18:29:19 +0530

    ui

--- client/src/components/ui/skeleton.tsx
+++ client/src/components/ui/skeleton.tsx
@@ -4,7 +4,10 @@ function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn(
+        "animate-pulse rounded-md bg-primary/10",
+        "bg-gray-700", 
+         className)}
       {...props}
     />
   )
